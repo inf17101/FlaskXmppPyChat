6c01ee7afe72797db2bc0b3f58fb84469ef0ba98
Oliver Klapper <oliver.klapper@web.de>
2020-04-01 11:19:32 +0200

    adds unittests for flask register route

---
+++ Tests/TestFlaskRegisterCase.py
@@ -0,0 +1,37 @@
+import unittest, sys
+from unittest.mock import patch
+import json
+sys.path.append("/home/xmppweb/XmppChat/xmppchat/")
+sys.path.append("/home/xmppweb/XmppChat/")
+from xmppchat.api import app
+from xmppchat.models import User
+
+
+
+class TestFlaskRegisterCase(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Method to set up things for testing
+        creates flask test client and important settings e.g. no csrf
+        """
+        app.config['WTF_CSRF_ENABLED'] = False
+        self.tester = app.test_client(self)
+
+
+
+    def test_register_page(self):
+        """
+        test correct get request on register page
+        """
+        response = self.tester.get('/register', content_type="html/text")
+        self.assertEqual(response.status_code, 200)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
